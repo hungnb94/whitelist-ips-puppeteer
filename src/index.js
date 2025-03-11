2ad9509fec1137784e4cc2c954070b2dac10051a
Hung Nguyen <hungnb94@gmail.com>
2025-03-11 20:08:32 +0700

    Fill ip address

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
 const puppeteer = require('puppeteer');
 const delay = require("./delay");
 
+const ipRanges = [
+    '103.21.244.0/22',
+    '103.22.200.0/22',
+    '103.31.4.0/22',
+    '104.16.0.0/13',
+    '104.24.0.0/14',
+    '108.162.192.0/18',
+    '131.0.72.0/22',
+    '141.101.64.0/18',
+    '162.158.0.0/15',
+    '172.64.0.0/13',
+    '173.245.48.0/20',
+    '188.114.96.0/20',
+    '190.93.240.0/20',
+    '197.234.240.0/22',
+    '198.41.128.0/17'
+];
+
+async function fillIpAddress(appFrame, index, ipaddress) {
+    console.log(index, 'address', ipaddress)
+    await appFrame.click('.oui-icon.oui-icon-add')
+    await appFrame.click('div > button.ui-select-match')
+    await appFrame.click(`button[id="ui-select-choices-row-0-${index}"]`)
+    await appFrame.select('td > span.oui-select > select[id="actionSelect"]', 'string:permit')
+    await appFrame.select('td > span.oui-select > select[id="protocolSelect"]', 'string:tcp')
+    await appFrame.type('input[id="source"]', ipaddress);
+    await appFrame.click('td > button[title="Validate"]')
+}
 
 async function main() {
     const userDataDir = "/Users/hung/Library/Application Support/Google/Chrome for Testing"
@@ -22,27 +50,17 @@ async function main() {
         {waitUntil: "load"}
     );
 
-    await page.setViewport({width: 1080, height: 1024});
+    await page.setViewport({width: 1260, height: 1024});
 
     console.log('wait iframe')
-    await delay(15000)
+    await delay(10000)
     const frameHandle = await page.waitForSelector('iframe[title="app"]')
     const appFrame = await frameHandle.contentFrame()
-
-    console.log('click add')
-    await appFrame.click('.oui-icon.oui-icon-add')
-    // await page.waitForSelector('::-p-text(Manage Edge Network Firewall rules)')
-    // await appFrame.click('.ui-select-match-text')
-    // await page.locator('.oui-icon .oui-icon-add').click();
-    // await page.locator('::-p-text(Add a rule)').click();
-    // await page.locator('text/My text').click();
-
-
-    // Locate the full title with a unique string
-    // const textSelector = await page.waitForSelector(
-    //     'text/Customize and automate',
-    // );
-    // const fullTitle = await textSelector?.evaluate(el => el.textContent);
+    for (let i = 0 ; i < ipRanges.length; i++) {
+        await fillIpAddress(appFrame, i, ipRanges[i])
+        await delay(30000)
+    }
+    await fillIpAddress(appFrame, 3, '')
     await delay(120000)
 
     await browser.close();
