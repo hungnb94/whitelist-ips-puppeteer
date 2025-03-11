0c5178f187374d92cb5bf898c96408a0f60eb238
Hung Nguyen <hungnb94@gmail.com>
2025-03-11 18:18:35 +0700

    Click add inside frame

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 const delay = require("./delay");
 
 
-async function main(){
+async function main() {
     const userDataDir = "/Users/hung/Library/Application Support/Google/Chrome for Testing"
 
     const browser = await puppeteer.launch({
@@ -15,15 +15,25 @@ async function main(){
         ]
     });
     const page = await browser.newPage();
+    const url = 'https://ca.ovh.com/manager/#/dedicated/ip/148.113.171.178/firewall?ip=148.113.171.178'
 
-    await page.goto('https://ca.ovh.com/manager/#/dedicated/ip/148.113.171.178/firewall?ip=148.113.171.178');
+    await page.goto(
+        url,
+        {waitUntil: "load"}
+    );
 
     await page.setViewport({width: 1080, height: 1024});
 
-    console.log('wait for load done')
-    await page.waitForSelector('::-p-text(Manage Edge Network Firewall rules)')
+    console.log('wait iframe')
+    await delay(15000)
+    const frameHandle = await page.waitForSelector('iframe[title="app"]')
+    const appFrame = await frameHandle.contentFrame()
+
     console.log('click add')
-    await page.locator('.oui-icon .oui-icon-add').click();
+    await appFrame.click('.oui-icon.oui-icon-add')
+    // await page.waitForSelector('::-p-text(Manage Edge Network Firewall rules)')
+    // await appFrame.click('.ui-select-match-text')
+    // await page.locator('.oui-icon .oui-icon-add').click();
     // await page.locator('::-p-text(Add a rule)').click();
     // await page.locator('text/My text').click();
 
@@ -33,6 +43,7 @@ async function main(){
     //     'text/Customize and automate',
     // );
     // const fullTitle = await textSelector?.evaluate(el => el.textContent);
+    await delay(120000)
 
     await browser.close();
 }
